d3515ae8c88994c4bcabb95f6cd2759080d4e898
Manikandan <manikandan19598@gmail.com>
2024-09-17 22:59:52 +0530

    Static Sidebar

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,10 @@
+<div id="mySidebar" class="sidebar">
+    <div class="sidebar-header">
+        <h3>Menu</h3>
+        <button class="toggle-btn" onclick="toggleNav()">
+            <i class="fas fa-bars"></i>
+        </button>
+    </div>
+    <a href="#"><i class="fas fa-chart-line"></i> <span> {{ ucwords(__('Dashboard')) }}</span></a>
+    <a href="#"><i class="fas fa-user"></i> <span> {{ ucwords(__('orders')) }}</span></a>
+</div>

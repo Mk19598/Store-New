f84b25581b30b1f56a8a27daa9219e74b99ef6d0
Manikandan <manikandan19598@gmail.com>
2024-09-17 23:08:27 +0530

    roles migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tiny_integer('active')->default(1);
+            $table->integer('role')->default(3);
             $table->rememberToken();
             $table->timestamps();
         });

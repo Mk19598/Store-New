3d3d81028433262d9f41d26e8821f89b9e563fc8
Manikandan <manikandan19598@gmail.com>
2024-09-21 23:37:19 +0530

    sidebar

--- resources/js/sidebar.js
+++ resources/js/sidebar.js
@@ -6,3 +6,16 @@ function toggleNav() {
         sidebar.classList.toggle("open");
     }
 }
+
+$(document).ready(function () {
+    $(".header-dropdown-button").click(function () {
+        $(".dropdown-menu").toggle();
+    });
+
+    $(document).click(function (event) {
+        if (!$(event.target).closest('.header-dropdown-dropdown').length) {
+            $(".dropdown-menu").hide();
+        }
+    });
+
+});
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,22 +3,50 @@
     $Get_website_name = CustomHelper::Get_website_name() ;
 @endphp
 
+                {{-- Sidebar --}}
+                
 <div id="mySidebar" class="sidebar">
     <div class="sidebar-header">
 
         <div class="row d-flex justify-content-center align-items-center">
-            <img src="{{ CustomHelper::Get_website_logo_url() }}" style="width: 40%;" alt="{{ $Get_website_name }}">
-            <small class="text-center" style="font-weight: bold;">  {{ ucwords(__( $Get_website_name )) }} </small>
+            <img src="{{ CustomHelper::Get_website_logo_url() }}" style="width: 40%;" alt="{{ $Get_website_name }}"><br><br><br>
+            {{-- <h6 id="website-name" class="text-center" style="font-weight: bolder;">{{ ucwords(__( $Get_website_name )) }}</h6> --}}
         </div>
 
-        {{-- <button class="toggle-btn" onclick="toggleNav()">
-            <i class="fas fa-bars"></i>
-        </button> --}}
+        <button class="toggle-btn" onclick="toggleNav()">
+            <i class="fa-solid fa-sliders"></i>
+        </button>   
     </div>
-    <a href="{{ route('orders.index') }}"><i class="fas fa-bars"></i> <span> {{ ucwords(__('Order List')) }}</span></a>
+    <a href="{{ route('orders.index') }}"><i class="fa-solid fa-list"></i> <span> {{ ucwords(__('Order List')) }}</span></a>
     <a href="{{ route('orders.store') }}"><i class="fas fa-cart-shopping"></i> <span> {{ ucwords(__('orders Store')) }}</span></a>
     <a href="{{ route('home') }}"><i class="fas fa-shopping-cart"></i> <span> {{ ucwords(__('products')) }}</span></a>
     <a href="{{ route('home') }}"><i class="fas fa-store"></i></i> <span> {{ ucwords(__('Inventary management')) }}</span></a>
     <a href="{{ route('home') }}"><i class="fas fa-truck-fast"></i> <span> {{ ucwords(__('Shipping')) }}</span></a>
-    
 </div>
+
+                {{-- Top Header --}}
+
+<header class="header-menus-site">
+    <nav class="d-flex justify-content-right align-items-center px-2 px-sm-3">
+        <div class="dropdown position-relative py-3">
+            <div class="header-dropdown-dropdown">
+                <div class="header-dropdown-button">
+                    <i class="fa-solid fa-user"></i>
+                    <span>{{ Auth::user()->name }}</span>
+                </div>
+                <div class="dropdown-menu">
+                    <ul>
+                        <li style="list-style: none;">
+                            <i class="fa-solid fa-right-from-bracket"></i> 
+                            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}</a>
+                        </li>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>

85a5911f9021598b9674611de9bd28d6edff9dba
Manikandan <manikandan19598@gmail.com>
2024-12-30 02:53:43 +0530

    Reset Password Email should be added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -57,11 +57,11 @@
                                     {{ __('Login') }}
                                 </button>
 
-                                {{-- @if (Route::has('password.request'))
+                                @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
-                                @endif --}}
+                                @endif
 
                                 {{-- <a class="btn btn-link" href="{{ route('register') }}">
                                     {{ __('Register Now') }}

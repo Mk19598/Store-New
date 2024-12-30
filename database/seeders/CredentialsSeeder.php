b3922e788c79ea3ec519234a637518b983960278
Sanjai Kumar M <sanjai@webnexs.in>
2024-12-30 10:39:18 +0530

    changes

--- database/seeders/CredentialsSeeder.php
+++ database/seeders/CredentialsSeeder.php
@@ -18,6 +18,7 @@ class CredentialsSeeder extends Seeder
         
         $Credentials_inputs = [
             [  'dukkan_api_token'  => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQxMTEyOTY4LCJqdGkiOiI0YTE1NTBiMmEzODk0Y2NhYjVjZmViMjc5MGQ1NGE5ZSIsInVzZXJfaWQiOjEzNzM4ODY1LCJoYXNfcGFzc3dvcmRfc2V0Ijp0cnVlLCJlbWFpbCI6ImR1a2FhbkBtYWlsaW5nMjQ3LmNvbSIsInVzZXJfYWN0aXZlIjp0cnVlLCJ1c2VyX2VtYWlsX3ZlcmlmaWVkIjp0cnVlLCJ1c2VybmFtZSI6ImR1a2FhbkB2YXVsdGQuZGUiLCJidXllcl9pZCI6MTM2NjkyNzYsInNlbGxlcl9pZCI6ODExOTU3NiwicGlsb3RfaWQiOjM3NzY3MzAsInZlbmRvcl9pZCI6bnVsbCwiYnV5ZXJfdXVpZCI6ImUxNjI2NzVmLWYxMmMtNDZiMC04MTRiLTQxMTdlODUxODc1ZiIsInNlbGxlcl91dWlkIjoiODNjNzVkZTUtOTVjZS00ZTJmLTk1NTYtOGQ3YTQxNzM1Njk1IiwicGlsb3RfdXVpZCI6ImE0MTc1MjcwLTdlOWItNGE0Yi05ZDNiLTE1ODJhOTRiZDJkMyIsInZlbmRvcl91dWlkIjpudWxsLCJ2ZW5kb3JfZGF0ZSI6bnVsbCwibXVsdGlwbGVfc3RvcmVzIjpmYWxzZSwic3RvcmVfdXVpZHMiOlsiMjYzODk4MmMtNTgzMi00YWM0LTk4MGUtZTYwNTYxYTU1ZWY4Il0sInN0b3JlX2lkcyI6WzEwMjY4NTU1MF0sInN0YWZmX2lkIjpudWxsLCJzdG9yZV90eXBlIjowLCJjdXN0b21fZGF0YSI6e319.35mVgOBtf8bGQdeVihIUAIlm0NO52Mm-MaOISg4gTrE', 
+               'dukkan_store_id'  => '987ba0ae-5a55-4732-8142-d8d9603bb74a',
                'woocommerce_url'  => 'https://standardcoldpressedoil.com',
                'woocommerce_customer_key' => 'ck_095510b173e2a3deb35f26e1a403f22d7ee2c60f',
                'woocommerce_secret_key'   =>  'cs_3865feb56d3de76b3eb3a6c865593b687f284488',

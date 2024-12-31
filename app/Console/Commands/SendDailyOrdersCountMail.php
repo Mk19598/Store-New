12ea92a1e3ba04122a560a06c005c05dbc84bc56
Manikandan <manikandan19598@gmail.com>
2024-12-31 23:03:32 +0530

    Cron

--- app/Console/Commands/SendDailyOrdersCountMail.php
+++ app/Console/Commands/SendDailyOrdersCountMail.php
@@ -8,7 +8,6 @@ use App\Mail\DailyOrdersCountMail;
 use App\Helpers\CustomHelper;
 use App\Models\Order;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\DB;
 
 class SendDailyOrdersCountMail extends Command
 {
@@ -32,23 +31,20 @@ class SendDailyOrdersCountMail extends Command
      */
     public function handle()
     {
-        // $ordersQuery = Order::query(); 
+        $ordersQuery = Order::query(); 
 
-        // $orders_today = $ordersQuery->whereDate('order_created_at', Carbon::now()->toDateString())->get();
+        $orders_today = $ordersQuery->whereDate('order_created_at', Carbon::now()->toDateString())->get();
 
-        // $data = [
-        //     'orders_today' => $orders_today,
-        //     'orders_count' => $orders_today->count(), 
-        //     'dukkan_orders_count' => $orders_today->where('order_vai', 'Dukkan')->count(), 
-        //     'woocommerce_orders_count' => $orders_today->where('order_vai', 'woocommerce')->count(),
-        //     'Get_website_logo_url'  => CustomHelper::Get_website_logo_url(),
-        //     'Get_website_name' => CustomHelper::Get_website_name(),
-        // ];
+        $data = [
+            'orders_today' => $orders_today,
+            'orders_count' => $orders_today->count(), 
+            'dukkan_orders_count' => $orders_today->where('order_vai', 'Dukkan')->count(), 
+            'woocommerce_orders_count' => $orders_today->where('order_vai', 'woocommerce')->count(),
+            'Get_website_logo_url'  => CustomHelper::Get_website_logo_url(),
+            'Get_website_name' => CustomHelper::Get_website_name(),
+        ];
         
-        // Mail::to(CustomHelper::Get_ADMIN_MAIL())->send(new DailyOrdersCountMail($data));
+        Mail::to(CustomHelper::Get_ADMIN_MAIL())->send(new DailyOrdersCountMail($data));
 
-       DB::table('user_roles')->insert(['role_name' => 1]);
-
-        $this->info('Daily email sent successfully!');
     }
 }
